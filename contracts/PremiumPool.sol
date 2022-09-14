e059d0a9761f4967d1c0a3dd6cf8db7c1de14873
Patrizio Stavola <patrizio.stavola@gmail.com>
2022-09-14 15:37:33 +0200

    implementing Aave lending pool deposit and interest-bearing token logic

--- contracts/PremiumPool.sol
+++ contracts/PremiumPool.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.13;
 
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./interfaces/ILendingPool.sol";
+import "./interfaces/IAToken.sol";
 import "./DrawController.sol";
 
 /**
@@ -19,10 +21,13 @@ contract PremiumPool is
     DrawController public draw; // draw controller instance
     IERC20 public usdc; // $USDC instance
     IERC20 public ticket; // ticket instance
+    ILendingPool public aPool; // aave usdc lending pool
+    IAToken public aToken; // aave interest bearing token
 
     mapping(address => uint256) public userIndex;
     uint256 public usersCount;
     address[] public users;
+    mapping(address => uint256) public userDepositedUsdc;
     uint256 public usdcDeposit;
     uint256 public prize;
 
@@ -33,9 +38,11 @@ contract PremiumPool is
 
     /* ========== CONSTRUCTOR ========== */
 
-    constructor(address _usdc, address _ticket, address vrfCoordinator, address link, bytes32 _keyhash, uint256 _fee) {
+    constructor(address _usdc, address _ticket, address _aPool, address _aToken, address vrfCoordinator, address link, bytes32 _keyhash, uint256 _fee) {
         usdc = IERC20(_usdc);
         ticket = IERC20(_ticket);
+        aPool = ILendingPool(_aPool);
+        aToken = IAToken(_aToken);
         draw = new DrawController(vrfCoordinator, link, _keyhash, _fee);
         users = new address[](0);
     }
@@ -48,15 +55,17 @@ contract PremiumPool is
     function deposit(uint256 _usdcAmount) public {
         require(_usdcAmount >= 100 * (10**18), "minimum deposit is 100 $USDC");
 
-        usdc.transferFrom(msg.sender, address(this), _usdcAmount);
-        ticket.transferFrom(address(this), msg.sender, _usdcAmount);
-        
         if(userIndex[msg.sender] == 0) {
             usersCount++;
+            users.push(msg.sender);
+            userIndex[msg.sender] = users.length;
         }
-        userIndex[msg.sender] += users.length;
-        users.push(msg.sender);
-
+        userDepositedUsdc[msg.sender] += _usdcAmount;
+        usdcDeposit += _usdcAmount;
+        usdc.transferFrom(msg.sender, address(this), _usdcAmount);
+        ticket.transferFrom(address(this), msg.sender, _usdcAmount);
+        aPool.deposit(address(usdc), _usdcAmount, 0);
+        
         emit Deposit(msg.sender, _usdcAmount);
     }
 
@@ -65,19 +74,19 @@ contract PremiumPool is
      * @param _usdcAmount usdc amount
      */
     function withdraw(uint256 _usdcAmount) public {
-        require(_usdcAmount >= 0, "cant withdraw 0 $USDC");
-        require(ticket.balanceOf(msg.sender) >= _usdcAmount, "user has not enough balance");
-        require(usdc.balanceOf(address(this)) >= _usdcAmount, "contract has not enough balance");
+        require(userDepositedUsdc[msg.sender] >= _usdcAmount, "You cannot withdraw more than deposited!");
 
         ticket.transferFrom(msg.sender, address(this), _usdcAmount);
+        aToken.redeem(_usdcAmount);
         usdc.transferFrom(address(this), msg.sender, _usdcAmount);
 
-        uint256 index = userIndex[msg.sender];
-        if(ticket.balanceOf(msg.sender) == 0){
-            delete users[index];
+        userDepositedUsdc[msg.sender] -= _usdcAmount;
+        if(userDepositedUsdc[msg.sender] == 0){
             usersCount--;
+            delete users[userIndex[msg.sender]];
+            userIndex[msg.sender] = 0;
         }
-        
+
         emit Withdraw(msg.sender, _usdcAmount);
     }
 
@@ -86,11 +95,14 @@ contract PremiumPool is
      */
     function pickWinner() public onlyOwner {
         uint256 currentDrawId = draw.drawId();
-        (, bool currentDrawIsOpen, , uint256 currentDrawEndTime, uint256 prize, uint256 usdcDeposit, ) = draw.draws(currentDrawId);
+        (, bool currentDrawIsOpen, , uint256 currentDrawEndTime, , , ) = draw.draws(currentDrawId);
 
         require(block.timestamp > currentDrawEndTime,"Draw endtime still not reached");
         require(!currentDrawIsOpen,"Draw already closed");
         require(usersCount != 0, "There has been no participation during this draw");
+
+        prize = aToken.balanceOf(address(this)) - usdcDeposit;
+        aToken.redeem(prize);
 
         draw.updatePrize(prize);
         draw.updateDeposit(usdcDeposit);
