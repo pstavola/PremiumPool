218f2ed048b42ee0434e8e00ca29ac1b79427669
Patrizio Stavola <patrizio.stavola@gmail.com>
2022-09-19 12:25:35 +0200

    forge configuration

--- src/DrawController.sol
+++ src/DrawController.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
-import "../node_modules/@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 import "./PremiumPool.sol";
 
 /**
