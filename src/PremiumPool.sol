be45328fd1993b388905d36cb57fa121859e5b49
Patrizio Stavola <patrizio.stavola@gmail.com>
2022-09-19 12:29:39 +0200

    forge configuration

--- src/PremiumPool.sol
+++ src/PremiumPool.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/ILendingPool.sol";
 import "./interfaces/IAToken.sol";
 import "./DrawController.sol";
--- src/Ticket.sol
+++ src/Ticket.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PremiumPoolTicket is ERC20, Ownable {
     constructor() ERC20("PremiumPoolTicket", "PPT") {}
