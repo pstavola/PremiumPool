79bf2eb69b6fb6482d25ecd619cd2f5782e341fa
Patrizio Stavola <patrizio.stavola@gmail.com>
2022-09-21 23:08:04 +0200

    adapted to ChainLink VRF v2, extended testPickWinner

--- test/PremiumPool.t.sol
+++ test/PremiumPool.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 import "../src/PremiumPool.sol";
 import "../src/DrawController.sol";
 import "../src/interfaces/IAToken.sol";
@@ -12,14 +13,15 @@ contract PremiumPoolTest is Test {
     PremiumPool public pool;
     DrawController public draw;
     PremiumPoolTicket public ticket;
+    VRFCoordinatorV2Mock public vrfCoordinator;
     uint256 forkId;
     address usdc = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     address aPool = address(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
     address aToken = address(0xBcca60bB61934080951369a648Fb03DF4F96263C);
-    address vrfCoordinator = address(0xf0d54349aDdcf704F77AE15b96510dEA15cb7952);
     address link = address(0x514910771AF9Ca656af840dff83E8264EcF986CA);
+    //address vrfCoordinator = address(0xf0d54349aDdcf704F77AE15b96510dEA15cb7952);
+    uint64 subscriptionId = 1;
     bytes32 keyhash = bytes32(0xAA77729D3466CA35AE8D28B3BBAC7CC36A5031EFDC430821C02BC31A238AF445);
-    uint256 fee = 2 * (10**18);
     IERC20 public usdcInstance = IERC20(usdc);
     IAToken public aTokenInstance = IAToken(aToken);
     address alice = makeAddr("alice");
@@ -28,11 +30,14 @@ contract PremiumPoolTest is Test {
 
     function setUp() public {
         forkId = vm.createSelectFork("https://mainnet.infura.io/v3/1fc7c7c3701c4083b769e561ae251f9a");
-        pool = new PremiumPool(usdc, aPool, aToken, vrfCoordinator, link, keyhash, fee);
+        vrfCoordinator = new VRFCoordinatorV2Mock(0, 0);
+        vrfCoordinator.createSubscription();
+        vrfCoordinator.fundSubscription(1, 7 ether);
+        pool = new PremiumPool(usdc, aPool, aToken, address(vrfCoordinator), link, subscriptionId, keyhash);
         draw = pool.draw();
+        vrfCoordinator.addConsumer(subscriptionId, address(draw));
         ticket = pool.ticket();
         pool.createNewDraw();
-        deal(address(link), address(draw), 2 * (10**18));
         deal(address(usdc), alice, 10000 * (10**18));
         deal(address(usdc), bob, 10000 * (10**18));
         deal(address(usdc), charlie, 10000 * (10**18));
@@ -188,14 +193,11 @@ contract PremiumPoolTest is Test {
     }
 
     // k. Can close the draw and pick a winner.
-    function testPickWinner(uint256 _aliceAmount, uint256 _bobAmount, uint256 _charlieAmount) public {
+    function testPickWinner(uint256 _aliceAmount, uint256 _bobAmount, uint256 _charlieAmount, uint256 _randomNum) public {
         _aliceAmount = bound(_aliceAmount, 100 * (10**18), 1000 * (10**18));
         _bobAmount = bound(_bobAmount, 1001 * (10**18), 5000 * (10**18));
         _charlieAmount = bound(_charlieAmount, 5001 * (10**18), 10000 * (10**18));
-
-        uint256 aliceBalance = usdcInstance.balanceOf(alice);
-        uint256 bobBalance = usdcInstance.balanceOf(bob);
-        uint256 charlieBalance = usdcInstance.balanceOf(charlie);
+        _randomNum = bound(_randomNum, 0, type(uint256).max);
 
         vm.prank(alice);
         pool.deposit(_aliceAmount);
@@ -205,16 +207,38 @@ contract PremiumPoolTest is Test {
         vm.roll(block.number+1);
         vm.prank(charlie);
         pool.deposit(_charlieAmount);
+        vm.roll(block.number+1);
         skip(24 hours);
         pool.pickWinner();
 
         uint256 currentDrawId = draw.drawId();
-        (, bool currentDrawIsOpen, , uint256 currentDrawEndTime, uint256 currentDrawPrize, uint256 currentDrawDeposit, address currentDrawWinner) = draw.draws(currentDrawId);
+        (, bool currentDrawIsOpen, , , uint256 currentDrawPrize, uint256 currentDrawDeposit, ) = draw.draws(currentDrawId);
 
         uint256 expectedPrize = aTokenInstance.balanceOf(address(pool)) - pool.usdcDeposit();
 
         assertEq(currentDrawPrize, expectedPrize);
         assertEq(currentDrawDeposit, pool.usdcDeposit());
         assertEq(currentDrawIsOpen, false);
+
+        uint256 aliceBalance = ticket.balanceOf(alice);
+        uint256 bobBalance = ticket.balanceOf(bob);
+        uint256 charlieBalance = ticket.balanceOf(charlie);
+
+        uint256[] memory randomWords = new uint256[](1);
+        randomWords[0] = _randomNum;
+        vm.prank(address(vrfCoordinator));
+        draw.rawFulfillRandomWords(1, randomWords);
+
+        (, , , , , , address currentDrawWinner) = draw.draws(currentDrawId);
+
+        assertFalse(currentDrawWinner==address(0));
+
+        if (currentDrawWinner == alice) {
+            assertEq(ticket.balanceOf(alice), aliceBalance + currentDrawPrize);
+        } else if (currentDrawWinner == bob) {
+            assertEq(ticket.balanceOf(bob), bobBalance + currentDrawPrize);
+        } else if (currentDrawWinner == charlie) {
+            assertEq(ticket.balanceOf(charlie), charlieBalance + currentDrawPrize);
+        }
     }
 }
