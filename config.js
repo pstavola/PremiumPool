4cbe47df454b439d3b0ee759c4009cb9e4ab42fb
Patrizio Stavola <patrizio.stavola@gmail.com>
2022-09-25 18:55:02 +0200

    added usdc contract address

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-
-  export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-  
+export const contractAddress = "0x6b39b761b1b64C8C095BF0e3Bb0c6a74705b4788"
+export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+export const contractUsdcaddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
