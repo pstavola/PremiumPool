9fd73314afebe5631616cb39ac87fbb4d4b6f3fe
Patrizio Stavola <patrizio.stavola@gmail.com>
2022-09-26 01:07:15 +0200

    deployment to goerli

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const contractAddress = "0x018313A634A75070074D3D1b6f1DD07d3d3057bd"
+export const contractAddress = "0xAeF19ee0bC2859FA68BF62f8D079250B3a95a911"
 export const ownerAddress = "0x20Df8B290c61094c1AE47827d03eB55e769eED9a"
 export const contractUsdcaddress = "0x07865c6E87B9F70255377e024ace6630C1Eaa37F"
